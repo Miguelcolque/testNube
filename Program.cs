4482e9f7bcb4da826169b045344cf454bcf791d3
Miguel Angel Colque Calizaya <141885574+Miguelcolque@users.noreply.github.com>
2025-11-19 09:38:33 -0400

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.WebHost.UseUrls("http://0.0.0.0.:8080");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,12 +9,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
@@ -23,3 +18,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
